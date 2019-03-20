35da5b7a29092c71c62e1d6b30451889b0a8b541
John Martel (RIT Student) <jwm3464@ad.rit.edu>
2019-03-20 14:46:30 -0400

    Moved on to step 15

--- server/app.js
+++ server/app.js
@@ -7,34 +7,34 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 
-const port = process.env.PORT||process.env.NODE_PORT||3000;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI||'mongodb://localhost/DomoMaker';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
-    if(err){
-        console.log('Could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 const router = require('./router.js');
-const app = experss();
-app.use('/assets',ewpress.static(path.resolve(`${__dirname}/../hosted/`)));
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true;
+  extended: true,
 }));
-app.engine('handlbars',expressHandlebars({defaultLayout: 'main'}));
-app.set('view engine','handlebars');
-app.set('views',`${__dirname}/../views`);
+app.engine('handlbars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
 
 router(app);
 
 app.listen(port, (err) => {
-    if(err){
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
 });
