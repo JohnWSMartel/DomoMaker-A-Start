76bd77e412f1fff9e24fffee4b3bf046c20b9f51
JohnWSMartel <jwm3464@g.rit.edu>
2019-03-21 07:46:49 -0400

    In step 20

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,23 +3,28 @@ const models = require('../models');
 const Account = models.Account;
 
 const loginPage = (req, res) => {
-    res.render('login');
+  res.render('login');
 };
 
 const signupPage = (req, res) => {
-    res.render('signup');
+  res.render('signup');
 };
 
 const logout = (req, res) => {
-    res.redirect('/');
+  res.redirect('/');
 };
 
 const login = (request, response) => {
-    
+
 };
 
 const signup = (request, response) => {
+    const req = request;
+    const res = response;
     
+    //cast to strings to cover up some security flaws
+    req.body.username = `${req.body.username}`;
+    req.body.pass = `${req.body.pass}`;
 };
 
 module.exports.loginPage = loginPage;
